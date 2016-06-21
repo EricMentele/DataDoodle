e2c7e89f8dcdad79cdeaef732ff3e93d776693d6
Eric Mentele <ementele@Erics-MacBook-Pro.local>
2016-06-21 09:28:58 -0700

    Added data model classes.

---
+++ DataDrivenTests/DataDrivenTests.m
@@ -0,0 +1,39 @@
+//
+//  DataDrivenTests.m
+//  DataDrivenTests
+//
+//  Created by Eric Mentele on 6/20/16.
+//  Copyright © 2016 Eric Mentele. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DataDrivenTests : XCTestCase
+
+@end
+
+@implementation DataDrivenTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
